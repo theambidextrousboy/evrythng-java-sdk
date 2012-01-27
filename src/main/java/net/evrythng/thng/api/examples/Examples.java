7bcf97871ff1bb68418863ad59b8907efdbd5024
Dominique Guinard <dev@guinard.org>
2012-01-27 18:22:41 +0100

    Refactor examples

--- src/main/java/net/evrythng/thng/api/examples/Examples.java
+++ src/main/java/net/evrythng/thng/api/examples/Examples.java
@@ -3,7 +3,7 @@
  * www.evrythng.net
  * 
  */
-package net.evrythng.thng.api.wrapper;
+package net.evrythng.thng.api.examples;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -16,6 +16,7 @@ import net.evrythng.thng.api.model.Property;
 import net.evrythng.thng.api.model.ThngCollection;
 import net.evrythng.thng.api.search.GeoCode;
 import net.evrythng.thng.api.search.SearchParameter.Type;
+import net.evrythng.thng.api.wrapper.ThngAPIWrapper;
 import org.apache.http.client.ClientProtocolException;
 
 /**
