12b909dfa003009076f625260c614421ceaeee94
Yi-Ming Lu <yi-ming@evrythng.com>
2013-07-22 10:50:48 +0100

    Add a model com.evrythng.thng.resource.model.store.User.Loyalty. Add a field loyalties in User.

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/User.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/User.java
@@ -4,12 +4,64 @@
  */
 package com.evrythng.thng.resource.model.store;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.evrythng.thng.resource.model.core.DurableResourceModel;
 
 /**
  * Model representation for <em>users</em>.
  */
 public class User extends DurableResourceModel {
+
+	/**
+	 * Model representation for user's loyalty data.
+	 */
+	public static class Loyalty {
+		private Integer points;
+		private Integer tierPoints;
+		private String tierLevel;
+		private Map<String, String> customFields;
+
+		public Integer getPoints() {
+			return points;
+		}
+
+		public void setPoints(Integer points) {
+			this.points = points;
+		}
+
+		public Integer getTierPoints() {
+			return tierPoints;
+		}
+
+		public void setTierPoints(Integer tierPoints) {
+			this.tierPoints = tierPoints;
+		}
+
+		public String getTierLevel() {
+			return tierLevel;
+		}
+
+		public void setTierLevel(String tierLevel) {
+			this.tierLevel = tierLevel;
+		}
+
+		public Map<String, String> getCustomFields() {
+			return customFields;
+		}
+
+		public void setCustomFields(Map<String, String> customFields) {
+			this.customFields = customFields;
+		}
+
+		public void addCustomFields(String key, String value) {
+			if (customFields == null) {
+				customFields = new HashMap<String, String>();
+			}
+			customFields.put(key, value);
+		}
+	}
 
 	/**
 	 * The user's unique email address.
@@ -64,6 +116,8 @@ public class User extends DurableResourceModel {
 	 * Tells if the user can log in or not.
 	 */
 	private Boolean canLogin = true;
+
+	private Map<String, Loyalty> loyalties;
 
 	/* *** Getters / Setters *** */
 
@@ -138,6 +192,14 @@ public class User extends DurableResourceModel {
 
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
+	}
+
+	public Map<String, Loyalty> getLoyalties() {
+		return loyalties;
+	}
+
+	public void setLoyalties(Map<String, Loyalty> loyalties) {
+		this.loyalties = loyalties;
 	}
 
 }
