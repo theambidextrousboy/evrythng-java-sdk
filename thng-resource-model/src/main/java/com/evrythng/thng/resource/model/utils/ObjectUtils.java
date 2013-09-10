1db824a74e598e2dc6c7b528e72fc91a8ef7c704
Michel <michel@evrythng.com>
2013-09-10 18:11:09 +0200

    Null-safe compare methods.

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/utils/ObjectUtils.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/utils/ObjectUtils.java
@@ -4,6 +4,8 @@
  */
 package com.evrythng.thng.resource.model.utils;
 
+import java.util.Comparator;
+
 /**
  * Provides utility method for objects.
  * 
@@ -21,4 +23,39 @@ public class ObjectUtils {
 		return a.equals(b);
 	}
 
+	/**
+	 * NOTE: <code>null</code> is treated as the smallest value.
+	 */
+	public static <T> int nullSafeCompare(T a, T b, Comparator<? super T> comp) {
+		if (a == null && b == null) {
+			return 0;
+		}
+		if (a == null) {
+			return -1;
+		}
+		if (b == null) {
+			return 1;
+		}
+		return normalizeCompareResult(comp.compare(a, b));
+	}
+
+	/**
+	 * NOTE: <code>null</code> is treated as the smallest value.
+	 */
+	public static <T extends Comparable<T>> int nullSafeCompare(T a, T b) {
+		if (a == null && b == null) {
+			return 0;
+		}
+		if (a == null) {
+			return -1;
+		}
+		if (b == null) {
+			return 1;
+		}
+		return normalizeCompareResult(a.compareTo(b));
+	}
+
+	private static int normalizeCompareResult(int x) {
+		return x > 0 ? 1 : (x < 0 ? -1 : 0);
+	}
 }
