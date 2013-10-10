6e00bb0fc2f0d4787384240223103698aa444288
Michel <michel@evrythng.com>
2013-10-10 09:39:53 +0200

    Formatted

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Place.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Place.java
@@ -5,35 +5,37 @@ import com.evrythng.thng.resource.model.store.geojson.GeoJson;
 
 /**
  * Model representation for a <em>place</em>
+ * 
  * @author colin
- *
+ * 
  */
 public class Place extends DurableResourceModel {
+
 	private String name;
 	private String description;
 	private String icon;
 	private GeoJson location;
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	public String getIcon() {
 		return icon;
 	}
-	
+
 	public void setIcon(String icon) {
 		this.icon = icon;
 	}
