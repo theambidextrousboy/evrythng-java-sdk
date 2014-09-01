5ba07a35c3a63e8d1f2abed195e9f219d98b3699
Jean Vaucher <jean@evrythng.com>
2014-09-01 21:08:59 +0100

    [ PORTAL-638 ] Csv output annotations : A bit of javadoc

--- evrythng-commons/src/main/java/com/evrythng/commons/annotations/csv/CsvTransient.java
+++ evrythng-commons/src/main/java/com/evrythng/commons/annotations/csv/CsvTransient.java
@@ -8,10 +8,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-/** 
- * 
- **/
 
+/**
+ * Used by the Csv MapperProvider.
+ * Annotates a getter method of a ResourceModel subclass. If so, the annotated
+ * column will not be rendered in by Csv MapperProvider.
+ **/
 @Target(value = ElementType.METHOD)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface CsvTransient {
