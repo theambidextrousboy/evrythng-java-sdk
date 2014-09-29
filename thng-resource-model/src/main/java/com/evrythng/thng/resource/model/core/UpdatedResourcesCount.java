211ea2c47d95bad768a419f7675c58d7d330277e
sollecitom <michele.sollecito@evrythng.com>
2014-09-29 10:26:48 +0100

    Fixed broken build

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/core/UpdatedResourcesCount.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/core/UpdatedResourcesCount.java
@@ -2,19 +2,18 @@
  * (c) Copyright 2014 EVRYTHNG Ltd London / Olten
  * www.evrythng.com
  */
-package com.evrythng.commons;
-
+package com.evrythng.thng.resource.model.core;
 
 /**
  * Return type of a bulk update operation for the different service. Contains
  * only the number of update results.
  * 
  **/
-public class BulkUpdate {
+public class UpdatedResourcesCount {
 
 	public final long updatedCount;
 
-	public BulkUpdate(long updatedCount) {
+	public UpdatedResourcesCount(long updatedCount) {
 		if (updatedCount < 0) {
 			throw new IllegalArgumentException("Negative : " + updatedCount);
 		}
