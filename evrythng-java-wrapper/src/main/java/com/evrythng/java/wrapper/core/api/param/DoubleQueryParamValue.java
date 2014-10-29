ba6d3b57f2e3aeb842f4c5dc22122eb98acda649
Aleksandr Dorogush <aleksandr.dorogush@evrythng.com>
2014-10-29 18:05:58 +0300

    ENGINE-713: Replaced for "radius" parameter.

---
+++ evrythng-java-wrapper/src/main/java/com/evrythng/java/wrapper/core/api/param/DoubleQueryParamValue.java
@@ -0,0 +1,17 @@
+/*
+ * (c) Copyright 2013 EVRYTHNG Ltd London / Zurich
+ * www.evrythng.com
+ */
+package com.evrythng.java.wrapper.core.api.param;
+
+import com.evrythng.java.wrapper.core.api.QueryParamValue;
+
+/**
+ * A {@link com.evrythng.java.wrapper.core.api.QueryParamValue} subclass that holds double value.
+ */
+public class DoubleQueryParamValue extends QueryParamValue {
+
+	public DoubleQueryParamValue(String key, double value) {
+		super(key, Double.toString(value));
+	}
+}
