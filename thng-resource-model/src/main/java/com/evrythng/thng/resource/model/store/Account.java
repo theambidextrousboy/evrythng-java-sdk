641dbdeafbcdf275c216f3dca58486317620a2d0
sollecitom <michele.sollecito@evrythng.com>
2014-11-11 17:18:59 +0000

    ENGINE-731: crud for Accounts in thng-store - integration test is missing (need to grand permissions in thng-access first)

---
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Account.java
@@ -0,0 +1,29 @@
+/*
+ * (c) Copyright 2014 EVRYTHNG Ltd London / Zurich
+ * www.evrythng.com
+ */
+package com.evrythng.thng.resource.model.store;
+
+import com.evrythng.thng.resource.model.core.DurableResourceModel;
+
+/**
+ * Model representation for <em>accounts</em>.
+ */
+public class Account extends DurableResourceModel {
+
+	private static final long serialVersionUID = -3583008548329971679L;
+	/**
+	 * The account's name.
+	 */
+	private String name;
+
+	public String getName() {
+
+		return name;
+	}
+
+	public void setName(final String name) {
+
+		this.name = name;
+	}
+}
