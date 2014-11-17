59a89c6f269c0f48538b1de0149583dbf67edcd5
sollecitom <michele.sollecito@evrythng.com>
2014-11-17 18:24:29 +0000

    Finalized the CRUD account operations in thng-store.
    ENGINE-731

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Account.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Account.java
@@ -16,6 +16,10 @@ public class Account extends DurableResourceModel {
 	 * The account's name.
 	 */
 	private String name;
+	/**
+	 * The account's image url.
+	 */
+	private String imageUrl;
 
 	public String getName() {
 
@@ -27,12 +31,23 @@ public class Account extends DurableResourceModel {
 		this.name = name;
 	}
 
+	public String getImageUrl() {
+
+		return imageUrl;
+	}
+
+	public void setImageUrl(final String imageUrl) {
+
+		this.imageUrl = imageUrl;
+	}
+
 	@Override
 	public String toString() {
 
 		StringBuilder sb = new StringBuilder("Account{");
 		sb.append("id='").append(getId()).append("\', ");
-		sb.append("name='").append(name).append("\'");
+		sb.append("name='").append(name).append("\', ");
+		sb.append("imageUrl='").append(imageUrl).append("\'");
 		sb.append('}');
 		return sb.toString();
 	}
