147d8db82f0b59df2b1b7cfe9af9bce0114b472f
Michel <michel@evrythng.com>
2014-12-09 15:17:01 +0000

    Fixes in the scope refactoring
    EA-35

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/core/ScopeResource.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/core/ScopeResource.java
@@ -5,108 +5,51 @@
 package com.evrythng.thng.resource.model.core;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * TODO write javadoc here
+ * Holds the scopes for a resource.
  */
 public class ScopeResource implements Serializable {
 
 	public static final String ALL_USERS = "allU";
 	public static final String ALL_PROJECTS = "allP";
-	public static final String ALL_DEVICES = "allD";
 
 	private static final long serialVersionUID = -1514722768762632206L;
-	private String[] users;
-	private String[] projects;
-	private String[] devices;
+	private List<String> users;
+	private List<String> projects;
 	public static final ScopeResource EMPTY = new ScopeResource();
 
 	public ScopeResource() {
 
-		users = new String[0];
-		projects = new String[0];
-		devices = new String[0];
+		users = new ArrayList<>();
+		projects = new ArrayList<>();
 	}
 
-	public ScopeResource(final String[] users, final String[] projects, final String[] devices) {
+	public ScopeResource(final List<String> users, final List<String> projects) {
 
 		this.users = users;
 		this.projects = projects;
-		this.devices = devices;
 	}
 
-	public String[] getUsers() {
+	public List<String> getUsers() {
 
-		return users != null ? Arrays.copyOf(users, users.length) : null;
+		return users;
 	}
 
-	public void setUsers(final String... users) {
+	public void setUsers(final List<String> users) {
 
 		this.users = users;
 	}
 
-	public String[] getProjects() {
+	public List<String> getProjects() {
 
-		return projects != null ? Arrays.copyOf(projects, projects.length) : null;
+		return projects;
 	}
 
-	public void setProjects(final String... projects) {
+	public void setProjects(final List<String> projects) {
 
 		this.projects = projects;
-	}
-
-	public String[] getDevices() {
-
-		return devices != null ? Arrays.copyOf(devices, devices.length) : null;
-	}
-
-	public void setDevices(final String... devices) {
-
-		this.devices = devices;
-	}
-
-	@Override
-	public boolean equals(final Object o) {
-
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof ScopeResource)) {
-			return false;
-		}
-
-		ScopeResource that = (ScopeResource) o;
-
-		if (!asSet(devices).equals(asSet(that.devices))) {
-			return false;
-		}
-		if (!asSet(projects).equals(asSet(that.projects))) {
-			return false;
-		}
-		if (!asSet(users).equals(asSet(that.users))) {
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-
-		int result = asSet(users).hashCode();
-		result = 31 * result + asSet(projects).hashCode();
-		result = 31 * result + asSet(devices).hashCode();
-		return result;
-	}
-
-	private Set<String> asSet(final String[] array) {
-
-		Set<String> set = new HashSet<>();
-		Collections.addAll(set, array);
-		return set;
 	}
 }
