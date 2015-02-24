54e91122b6a3281f6c1fc274e4484ad9638f5dab
sollecitom <michele.sollecito@evrythng.com>
2015-02-24 14:02:21 +0000

    Trying to fix ProductApiITest.deleteProduct

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/li/UrlBinding.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/li/UrlBinding.java
@@ -158,18 +158,4 @@ public class UrlBinding extends AbstractUrlBinding {
 		}
 		return sb.toString();
 	}
-
-	@Override
-	public String toString() {
-
-		// TODO _MS_ remove
-		return "{" +
-				"data='" + data + '\'' +
-				", shortId='" + shortId + '\'' +
-				", evrythngId='" + evrythngId + '\'' +
-				", hits=" + hits +
-				", image='" + image + '\'' +
-				", format='" + format + '\'' +
-				super.toString();
-	}
 }
