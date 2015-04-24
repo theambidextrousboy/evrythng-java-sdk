c21cf9a9f6db4c24d8f9d6dfa3dbbb06e45d7349
Aleksandr Dorogush <aleksandr.dorogush@evrythng.com>
2015-04-24 17:08:53 +0300

    Fixed after code review.
    ENGINE-1162, ENGINE-1163

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/RedirectorRules.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/RedirectorRules.java
@@ -25,31 +25,4 @@ public class RedirectorRules extends DurableResourceModel {
 
 		this.rules = rules;
 	}
-
-	@Override
-	public boolean equals(final Object o) {
-
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		if (!super.equals(o)) {
-			return false;
-		}
-
-		RedirectorRules that = (RedirectorRules) o;
-
-		return !(rules != null ? !rules.equals(that.rules) : that.rules != null);
-
-	}
-
-	@Override
-	public int hashCode() {
-
-		int result = super.hashCode();
-		result = 31 * result + (rules != null ? rules.hashCode() : 0);
-		return result;
-	}
 }
